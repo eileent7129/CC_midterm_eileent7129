f4c484d855ddac7d796c840d6967b9760088f5af
eileent7129 <eat7129@nyu.edu>
2022-11-03 12:22:28 -0400

    changes to circle pattern
    added more changes to circle pattern, trying to make spiral.

--- Midterm_1/p5_starter/sketch.js
+++ Midterm_1/p5_starter/sketch.js
@@ -14,26 +14,31 @@ let new_shape;
 //let my_circle;
 let my_color;
 let angle;
-let circle1;
-let circle2;
-let circle3;
-let circle4;
-let circle5;
-let circle6;
-let circle7;
-let circle8;
-let circle9;
-let circle10;
-let circle11;
-let circle12;
+//let circle1;
+//let circle2;
+//let circle3;
+//let circle4;
+//let circle5;
+//let circle6;
+//let circle7;
+//let circle8;
+//let circle9;
+//let circle10;
+//let circle11;
+//let circle12;
 let opacity;
+let radius;
+let array1;
+let array2;
 
 function setup() {
     createCanvas(800, 800);
-    background(253, 253, 203);
+    background(253, 183, 246);
     //logo = new MovingLogo();
+    frameRate(30);
     x = 0;
-    y =  -300;
+    y = -300;
+    radius = 300;
     my_color = color(255);
     angle = 0;
     opacity = 10;
@@ -41,71 +46,19 @@ function setup() {
     //rectangle = new Rectangle(x, y);
     //my_triangle = new MyTriangle(x, y);
     new_shape = 1;
-    //my_circle = new Circle(x, y, my_color);
-    circle1 = new Circle(0, -300, my_color);
-    circle2 = new Circle(300*(1 / 2), 300*(sqrt(3) / 2), my_color);
-    circle3 = new Circle(300*(sqrt(3) / 2), 300*(1 / 2), my_color);
-    circle4 = new Circle(300, 0, my_color);
-    circle5 = new Circle(300*(sqrt(3) / 2), -300*(1 / 2), my_color);
-    circle6 = new Circle(300 * (1 / 2), -300 * (sqrt(3) / 2), my_color);
-    circle7 = new Circle(0, 300, my_color);
-    circle8 = new Circle(-300 * (1 / 2), -300 * (sqrt(3) / 2), my_color);
-    circle9 = new Circle(-300 * (sqrt(3) / 2), -300 * (1 / 2), my_color);
-    circle10 = new Circle(-300, 0, my_color);
-    circle11 = new Circle(-300 * (sqrt(3) / 2), 300 * (1 / 2), my_color);
-    circle12 = new Circle(-300 * (1 / 2), 300 * (sqrt(3) / 2), my_color);
+    array1 = circlePattern(radius, my_color);
+    array2 = circlePattern(radius - 100, my_color);
+
 }
 
 function draw() {
     //logo.move();
     //logo.display();
-    /* background(253, 253, 203);*/
+
+    displayPattern(array1, opacity);
+    displayPattern(array2, opacity);
+
     translate(width / 2, height / 2);
-    circle1.display();
-    circle1.change_color(opacity);
-
-    circle2.display();
-    circle2.change_color(opacity);
-
-    circle3.display();
-    circle3.change_color(opacity);
-
-    circle4.display();
-    circle4.change_color(opacity);
-
-    circle5.display();
-    circle5.change_color(opacity);
-
-    circle6.display();
-    circle6.change_color(opacity);
-
-    circle7.display();
-    circle7.change_color(opacity);
-
-    circle8.display();
-    circle8.change_color(opacity);
-
-    circle9.display();
-    circle9.change_color(opacity);
-
-    circle10.display();
-    circle10.change_color(opacity);
-
-    circle11.display();
-    circle11.change_color(opacity);
-
-    circle12.display();
-    circle12.change_color(opacity);
-
-    //for (let i = 0; i <= 12; i++) {
-    //    rotate(radians(angle));
-    //    my_circle.display();
-    //    //my_circle.change_color();
-    //    print("this.x: "); print(my_circle.xPos()); print('\n');
-    //    print("this.y: "); print(my_circle.yPos()); print('\n');
-    //    print("angle: "); print(angle); print('\n');
-    //    angle += 30;
-    //}
 }
 
 class Circle {
@@ -132,6 +85,25 @@ class Circle {
         }
     }
 
+    blotch() {
+
+        strokeWeight(2);
+        stroke(255);
+        line(this.x - 25, this.y, this.x + 25, this.y);
+        line(this.x -10, this.y + 15, this.x +10, this.y + 10); 
+        line(this.x, this.y + 6, this.x - 5, this.y);
+        line(this.x + 10, this.y, this.x - 10, this.y );
+        line(this.x - 10, this.y - 10, this.x + 5, this.y + 10);
+        line(this.x + 10, this.y + 10, this.x - 7, this.y + 3);
+        line(this.x + 2, this.y + 2, this.x + 9, this.y + 9);
+        line(this.x + 6, this.y + 1, this.x + 2, this.y + 12);
+        line(this.x - 2, this.y + 8, this.x - 5, this.y + 2);
+        line(this.x + 12, this.y + 3, this.x - 15, this.y + 2);
+        line(this.x - 20, this.y - 5, this.x + 5, this.y + 10);
+        line(this.x + 5, this.y + 10, this.x-10, this.y  + 3);
+        line(this.x + 5, this.y - 15, this.x - 10, this.y - 3);
+    }
+
     xPos() {
         return this.x;
     }
@@ -139,5 +111,32 @@ class Circle {
     yPos() {
         return this.y;
     }
+}
 
+function circlePattern(my_radius, my_color) {
+    let circle1 = new Circle(0, -my_radius, my_color);
+    let circle2 = new Circle(my_radius * (1 / 2), my_radius * (sqrt(3) / 2), my_color);
+    let circle3 = new Circle(my_radius * (sqrt(3) / 2), my_radius * (1 / 2), my_color);
+    let circle4 = new Circle(my_radius, 0, my_color);
+    let circle5 = new Circle(my_radius * (sqrt(3) / 2), -my_radius * (1 / 2), my_color);
+    let circle6 = new Circle(my_radius * (1 / 2), -my_radius * (sqrt(3) / 2), my_color);
+    let circle7 = new Circle(0, my_radius, my_color);
+    let circle8 = new Circle(-my_radius * (1 / 2), -my_radius * (sqrt(3) / 2), my_color);
+    let circle9 = new Circle(-my_radius * (sqrt(3) / 2), -my_radius * (1 / 2), my_color);
+    let circle10 = new Circle(-my_radius, 0, my_color);
+    let circle11 = new Circle(-my_radius * (sqrt(3) / 2), my_radius * (1 / 2), my_color);
+    let circle12 = new Circle(-my_radius * (1 / 2), my_radius * (sqrt(3) / 2), my_color);
+
+    let circle_array = [];
+    circle_array.push(circle1, circle2, circle3, circle3, circle4, circle5, circle6, circle7, circle8, circle9, circle10, circle11, circle12);
+    return circle_array;
+    
+}
+
+function displayPattern(circle_array, my_opacity) {
+
+    for (let i = 0; i < circle_array.length; i++) {
+        circle_array[i].display();
+        circle_array[i].change_color(my_opacity);
+    }
 }
