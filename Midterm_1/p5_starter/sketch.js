a6ebac101a028c88c8c1b0897b398d3e07bc7a9f
eileent7129 <eat7129@nyu.edu>
2022-11-09 08:01:08 -0500

    domino scene
    finished domino scene

--- Midterm_1/p5_starter/sketch.js
+++ Midterm_1/p5_starter/sketch.js
@@ -10,14 +10,14 @@
 let square;
 let rectangle;
 let my_triangle;
-let x;
-let y;
+//let x;
+//let y;
 let new_shape;
 let my_color;
 
 // circle pattern
 let angle1;
-let angle2;
+//let angle2;
 let radius;
 let array1;
 let array2;
@@ -33,8 +33,8 @@ let change;
 //domino
 let domino;
 let domino2;
-let x;
-let y;
+let domino3;
+let dominoSystem = [];
 
 function setup() {
     createCanvas(800, 800);
@@ -42,53 +42,148 @@ function setup() {
     frameRate(30);
     radius = 480;
     my_color = color(255);
-    angle1 = 0;
-    angle2 = 0;
+    angle = 0;
     idx = 0;
     next = 1;
-    x = width / 2;
-    y = height / 2; 
+    origin_x = width / 2;
+    origin_y = height / 2;
+    fall = false;
     //square = new Square(x, y);
     //rectangle = new Rectangle(x, y);
     //my_triangle = new MyTriangle(x, y);
     new_shape = 1;
     change = 1;
-    domino = new Domino(-10, -50);
-    domino2 = new Domino(0, -50);
 
+    //for (let x = 20; x < width; x += 60) {
+    //    domino = new Domino(-10, -50);
+    //    dominoArray.push(domino);
+    //}
+
+    for (let y = 60; y < height - 10; y += 120) {
+        dominoSystem.push(new DominoSystem(y));
+    }
+   
 }
 
 function draw() {
     //logo.move();
     //logo.display();
 
-    //background(39, 0, 90); // purple
     background(0);
-    translate(x, y);
-    rotate(radians(angle1));
-    domino.display();
 
-    if (angle1 == 90) {
-        angle1 = 90;
+    //when frameCount % 5 == 0, this should trigger the animation of the domino to fall down 
+    // try using an array for the dominoes
+
+
+    for (let i = 0; i < dominoSystem.length; i++) {
+        dominoSystem[i].addDomino();
+
+        if (i == 0) {
+            dominoSystem[i].run(true);
+        }
+        else {
+            dominoSystem[i].run(dominoSystem[i-1].finished());
+        }
+       
     }
-    else {
-        angle1++;
-    }
-   
 }
 class Domino {
     //diplay method
     //fall down method
-    //   if touching, fall down7
+    //  if touching, fall down
 
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        this.angle = 0;
     }
 
     display() {
         fill(255);
         noStroke();
         rect(this.x, this.y, 10, 50);
+    }
+
+    run(start) {
+        if (start) {
+            this.fall();
+        }
+        this.display();
+       
+    }
+
+    fall() { // tipping animation for domino (finally works :') )
+        if (this.angle == 75) {
+            this.angle = 75;
+        }
+        else {
+            this.angle+=5;
+        }
+        rotate(radians(this.angle));
+    }
+         
+
+    my_angle() {
+        return this.angle;
+    }
+
+    set_angle(new_angle) {
+        this.angle = new_angle;
+    }
+
+    is_touching() {
+        if (this.angle > 35) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    disappear() {
+        fill(0);
+        noStroke();
+        rect(this.x, this.y, 10, 50);  
+    }
+
+    // check if top right corner of is touching left wall of next domino
+    // if touching, let next domino fall
+}
+
+class DominoSystem {
+    constructor(yPos) {
+        this.yPos = yPos;
+        this.dominos = [];
+        this.done = false;
+    }
+
+    addDomino() {
+        this.dominos.push(new Domino(-10, -50));
+    }
+
+    run(start) {
+        // there needs to be some kind of trigger that the last domino had fallen so the next one can fall
+        if (start) {
+            let x = 20;
+            for (let i = 0; i < this.dominos.length; i++) {
+                applyMatrix();
+                translate(x, this.yPos);
+
+                if (i == 0) {
+                    this.dominos[i].run(start);
+                }
+                else {
+                    this.dominos[i].run(this.dominos[i - 1].is_touching());
+                }
+                x += 40;
+                this.done = true;
+                resetMatrix();
+            }
+
+        }
+       
+    }
+    finished() {
+        return this.done;
     }
 }
